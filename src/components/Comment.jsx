c3ca42ceb6022a3f94003380dcc56b635d624e60
Drayerr <gabrielgrv2015@outlook.com>
2022-09-07 16:05:27 -0300

    Add comment component.

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/ChrisCoy.png"/>
+      
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Christopher Lee</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Drayerr, voa mlk!</p>
+
+        </div>
+
+        <footer className={styles.footer}>
+          <button>
+            <ThumbsUp size={20}/>
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
