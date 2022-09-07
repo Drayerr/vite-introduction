0893b6e6a527f7120843810bd6092e16ee1aaa50
Drayerr <gabrielgrv2015@outlook.com>
2022-09-07 11:08:19 -0300

    Started making sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import styles from "./App.module.css"
 import "./global.css"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
-
-      <Post
-        author="Gabriel"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam amet labore dolores quod suscipit earum assumenda unde maiores eligendi id. Fugiat ratione, suscipit corporis voluptates blanditiis eaque harum omnis dolorum."
-      />
-
+      {/* o wrapper está separando os componentes internos em duas colunas, configurado no app.module.css */}
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Gabriel"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam amet labore dolores quod suscipit earum assumenda unde maiores eligendi id. Fugiat ratione, suscipit corporis voluptates blanditiis eaque harum omnis dolorum."
+          />
+        </main>
+      </div>
     </div>
   )
 }
